697f859d2c318d55e974196a021f1dd13fd1cc6c
Widwards <theWidwards@gmail.com>
2013-05-08 02:29:34 -0700

    added algorithm Fibonacci

---
+++ Visual Studio 2012/MyAlgorithmRealization/AlgorithmUsing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WidwardsAlgorithmsLibrary;
+
+namespace MyAlgorithmRealization
+{
+    class AlgorithmUsing
+    {
+        //Правила:
+        // 0. Мультипликативные константы можно опускать
+        // 1. n^a растет быстрее, чем n^b, при a>b
+        // 2. Любая экспонента растет быстрее любого полинома
+        // 3. Любой полином растет быстрее любого полилогарифма
+
+
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+
+        
+
+        
+        
+        /// <summary>
+        /// <para>
+        /// O(n^2)
+        /// </para>
+        /// 0, 1, 1, 2, 3, 5, 8, 13...
+        /// <para>
+        /// экспоненциальная скорость роста
+        /// </para>
+        /// Fn=2^0.7n
+        /// <para>
+        /// Время работы работы T(n) >= T(n-1)+T(n-2)+3
+        /// </para>
+        /// </summary>
+
+        private static void FibUsing()
+        {
+            Console.WriteLine("fib(n)   enter n:");
+            try
+            {
+                var fibonacci = WidwardsBasicAlgorithms.FibocacciLong(int.Parse(Console.ReadLine()));
+                foreach (var f in fibonacci)
+                {
+                    Console.Write(f.ToString() + " ");
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+    }
+}
---
+++ Visual Studio 2012/WidwardsAlgorithmsLibrary/WidwardsBasicAlgorithms.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidwardsAlgorithmsLibrary
+{
+    public static class WidwardsBasicAlgorithms
+    {
+
+
+        public static List<int> Fibonacchi(int n)
+        {
+            List<int> fib = new List<int>();
+            if (n < 0)
+            {
+                throw new Exception("Error! n<0");
+            }
+            if (n == 0)
+            {
+                return fib;
+            }
+            fib.Add(0);
+            if (n == 1)
+            {
+                return fib;
+            }
+            fib.Add(1);
+            for (int i = 1; i < n - 1; i++)
+            {
+                fib.Add(fib[i] + fib[i - 1]);
+            }
+            
+            
+            return fib;
+        }
+        public static List<ulong> FibocacciLong(int n)
+        {
+            
+            List<ulong> fib = new List<ulong>();
+            if (n < 0)
+            {
+                throw new Exception("Error! n<0");
+            }
+            if (n == 0)
+            {
+                return fib;
+            }
+            fib.Add(0);
+            if (n == 1)
+            {
+                return fib;
+            }
+            fib.Add(1);
+            for (int i = 1; i < n - 1; i++)
+            {
+                fib.Add(fib[i] + fib[i - 1]);
+            }
+            return fib;
+        }
+    }
+}
